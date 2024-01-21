d92e2c8ebe5224cc84475a0019b72a167b6e25d3
Mariam <kelanimaryam1234@gmail.com>
2024-01-21 06:25:32 -0800

    bubble sort

--- bub_sort.c
+++ bub_sort.c
@@ -4,21 +4,28 @@
  * bubble_sort - program that sorts an array of integers
  * @array: array of integers to be sorted
  * @size: size of the array
- * 
  * Return: void
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t index_var, j;
 	int tmp;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (index_var = 0; index_var < size - 1; index_var++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - index_var - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
